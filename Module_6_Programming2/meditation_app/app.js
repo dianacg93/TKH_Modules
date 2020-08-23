c4a8f19555d8ff47cb845d206d5953055564bafe
DianaG <diana.cg93@gmail.com>
2020-08-23 17:54:29 -0400

    fixing error with meditation app folder

---
+++ Module_6_Programming2/meditation_app/app.js
@@ -0,0 +1,78 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    //Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    //Get the length of the outline
+    const outlineLength = outline.getTotalLength();
+    //Duration
+    let fakeDuration = 600;
+    const timeSelect = document.querySelectorAll('.time-select button');
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    //Select Sound
+    sounds.forEach(function(sound){
+        sound.addEventListener('click', function(){
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            songPlayToggle(song);
+        })
+    })
+
+    //Play Sound
+    play.addEventListener('click', function() {
+        songPlayToggle(song);
+    })
+
+    //Sounds Play Toggle
+    const songPlayToggle = song => {
+        if(song.paused){
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        }else{
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+    }
+
+    //Animate circle
+    song.ontimeupdate = function() {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        // Animate Circle
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        //Animate Text
+        timeDisplay.textContent = `${minutes}: ${seconds}`;
+
+        if(currentTime >= fakeDuration){
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
+        }
+    }
+
+    //Select Sound Duration
+    timeSelect.forEach(function(option) {
+        option.addEventListener('click', function(){
+            fakeDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(fakeDuration/60)}:${Math.floor(fakeDuration%60)}`;
+        })
+    })
+};
+
+app();
